4ea1fdb5f59636347d8f743ab29e6db36706c2dd
Crowdbotics <team@crowdbotics.com>
2022-05-23 11:04:19 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "viaweb_35394",
+  url: "https://viaweb_35394.botics.co",
+  api: "https://viaweb_35394.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
